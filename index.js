31d997ee7d0dc5bb6347207ab925c94493e5e5b8
JaynHope <jaynhope1@gmail.com>
2025-08-11 14:14:59 -0700

    added toggle for nav n removed text-shadow

--- index.js
+++ index.js
@@ -3,6 +3,33 @@ const hamburger = document.querySelector('.js_hamburger');
 const first = document.querySelector('.js_first');
 const mid = document.querySelector('.js_mid');
 const last = document.querySelector('.js_last');
+const navLinks = Object.values(document.getElementsByTagName('a'));
+
+// const navL = 
+console.log(typeof(navLinks))
+
+// Function for toggling the navlinks
+navLinks.forEach((button) => {
+    button.addEventListener('click', () => {
+        //had to change it to activ instead of active cause active is already performing a function.
+            if (button.classList.contains('activ')) {
+                button.classList.remove('activ');
+            }
+             else {
+                turnOffPreviousButton();
+                button.classList.add('activ');
+             }
+        }
+    )
+})
+
+function turnOffPreviousButton() {
+    const previousButton = document.querySelector('.active');
+    if (previousButton) {
+        previousButton.classList.remove('active');
+    }
+}
+
 
 hamburger.addEventListener('click', () => {
 
